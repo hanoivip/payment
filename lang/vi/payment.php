beb5e395581b72f06aefc5c034fc01a1908aec53
wartune <wartune.mobi@gmail.com>
2021-08-06 15:28:11 +0700

    f

---
+++ lang/vi/payment.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'methods' => [
+        'next' => 'Chọn',
+        'title' => 'Chọn phương thức thanh toán',
+        'empty' => 'Hiện chưa có cách nào để thanh toán, vui lòng quay lại sau!'
+    ],
+];
--- views/new-topup-methods.blade.php
+++ views/new-topup-methods.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 
 @if (count($methods) > 0)
+<h1>{{__('hanoivip::payment.methods.title')}}</h1>
 <form method="post" action="{{route('newtopup.choose')}}">
 {{ csrf_field() }}
 	<input type="hidden" id="order" name="order" value="{{$order}}"/>
@@ -13,10 +14,10 @@
 		<input type="radio" id="method" name="method" value="{{$code}}"/>
 		<p>{{$method['name']}}</p>
 	@endforeach
-	<button type="submit">Next</button>
+	<button type="submit">{{__('hanoivip::payment.methods.next')}}</button>
 </form>	
 @else
-	<p>__('hanoivip::payment.methods.no-method')</p>
+	<p>{{__('hanoivip::payment.methods.empty')}}</p>
 @endif
 
 @endsection
