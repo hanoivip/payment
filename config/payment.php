5ac5a7f3f1eaf9f54c67f9488778d489838e236e
wartune <wartune.mobi@gmail.com>
2021-08-07 10:45:39 +0700

    f

--- config/payment.php
+++ config/payment.php
@@ -6,9 +6,6 @@ return [
         'uri' => 'http://payment-host.test/',
         'key' => '',
     ],
-    'declare_wrong_cutoff' => 50,
-    'enabled_types' => ['VTT', 'VNP'],
-    'cutoffs' => ['VTT' => 'viettelCutoffs', 'VNP' => 15],
     'methods' => [
         'credit' => [ 'name' => __('hanoivip::payment.credit'), 'service' => 'CreditPaymentMethod', 'need_config' => false, 
             'enable' => true],// otp? captcha?
@@ -19,4 +16,29 @@ return [
         'momo1' => ['name' => __('hanoivip::payment.momo'), 'service' => 'MomoPaymentMethod', 'need_config' => false, 
             'enable' => true],
     ],
+    'tsr' => [
+        'telco' => [
+            'VTT' => [ 'available' => true, 'title' => 'Viettel Card', 'need_dvalue' => true,
+                'supported_values' => [50000 => '50k', 100000 => '100k', 200000 => '200k', 300000 => '300k', 500000 => '500k']],
+            'VNP' => [ 'available' => true, 'title' => 'Vinaphone Card', 'need_dvalue' => true,
+                'supported_values' => [20000 => '20k', 30000 => '30k', 50000 => '50k', 100000 => '100k', 200000 => '200k', 300000 => '300k', 500000 => '500k']],
+            'VMS' => [ 'available' => false, 'title' => 'Mobifone Card', 'need_dvalue' => true,
+                'supported_values' => [50000 => '50k', 100000 => '100k', 200000 => '200k', 300000 => '300k', 500000 => '500k']],
+            'GATE' => [ 'available' => true, 'title' => 'FPT GATE', 'need_dvalue' => true,
+                'supported_values' => [10000 => '10k', 20000 => '20k', 30000 => '30k', 50000 => '50k',
+                    100000 => '100k', 200000 => '200k', 300000 => '300k', 500000 => '500k', 1000000 => '1000k']],
+            'VNG' => [ 'available' => true, 'title' => 'Zing Card', 'need_dvalue' => true,
+                'supported_values' => [10000 => '10k', 20000 => '20k', 30000 => '30k', 50000 => '50k',
+                    100000 => '100k', 200000 => '200k', 300000 => '300k', 500000 => '500k', 1000000 => '1000k']],
+            //'MOMO' => [ 'available' => false, 'title' => 'Ví Momo', 'need_dvalue' => false ],
+        ],
+        'telco_static' => [
+            'VTT' => 'Thẻ Viettel',
+            'VNP' => 'Thẻ Vinaphone',
+            'VMS' => 'Thẻ Mobifone',
+            'GATE' => 'Thẻ FPT GATE',
+            'VNG' => 'Thẻ Vinagame',
+        ],
+        'values_static' => [50000 => '50k', 100000 => '100k', 200000 => '200k', 300000 => '300k', 500000 => '500k'],
+    ]
 ];
--- views/new-topup-method-tsr1.blade.php
+++ views/new-topup-method-tsr1.blade.php
@@ -12,14 +12,14 @@
 {{ csrf_field() }}
 	<input type="hidden" id="trans" name="trans" value="{{$trans}}"/>
 	Loại thẻ: <select id="cardtype" name="cardtype">
-		@foreach (__('hanoivip::tsr.telco_static') as $cardtype => $title))
+		@foreach (Config::get('payment.tsr.telco_static') as $cardtype => $title))
 			<option value="{{$cardtype}}">{{$title}}</option>
 		@endforeach 
 	</select>
 	Seri thẻ: <input id="cardseri" name="cardseri" value=""/>
 	Mã thẻ: <input id="cardpass" name="cardpass" value=""/>
 	Giá trị thẻ: <select id="dvalue" name="dvalue">
-		@foreach (__('hanoivip::tsr.values_static') as $value => $title)
+		@foreach (Config::get('payment.tsr.values_static') as $value => $title)
 			<option value="{{$value}}">{{$title}}</option>
 		@endforeach
 	</select>
