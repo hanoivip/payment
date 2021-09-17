fc2edd312fcc534fa49dcb2811a861937ab3a657
hanoivip <game.oh.vn@gmail.com>
2021-09-17 10:08:06 +0700

    f

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ Route::middleware([
     'web',
     'auth:web'
 ])->namespace('Hanoivip\Payment\Controllers')->group(function () {
-    
-    // gate default path
-    //Route::get('/topup', 'TopupController@topupUI2');
-    //Route::get('/history', 'TopupController@history')->name('history');
-    
     // new gate version
     Route::get('/topup/step1', 'TopupController@topupUI2')->name('topup');
     Route::get('/topup/step2', 'TopupController@selectType')->name('topup.by.type');
@@ -21,15 +16,13 @@ Route::middleware([
     Route::get('/jhistory', 'TopupController@jsHistory')->name('jhistory');
     Route::get('/jrecharge', 'TopupController@jsRecharge')->name('jrecharge');
     // tracking topup
-    Route::get('/topup/success', 'TopupController@onTopupSuccess')->name('topup.success');
-    
+    Route::get('/topup/success', 'TopupController@onTopupSuccess')->name('topup.success');  
     // 20210728: new flow
     Route::get('/new/topup', 'NewTopup@listMethods')->name('newtopup');
     Route::post('/new/topup/shop', 'NewTopup@showShop')->name('newtopup.shop');
     Route::post('/new/topup', 'NewTopup@choose')->name('newtopup.choose');
     Route::any('/new/topup/do', 'NewTopup@topup')->name('newtopup.do');
     Route::any('/new/topup/query', 'NewTopup@query')->name('newtopup.query');
-    //Route::any('/new/topup/history', 'NewTopup@history')->name('newtopup.history');
 });
 
 Route::middleware([
@@ -38,14 +31,4 @@ Route::middleware([
 ])->namespace('Hanoivip\Payment\Controllers')
     ->prefix('ecmin')
     ->group(function () {
-    // List all current policies
-    Route::get('/policy', 'PolicyController@list')->name('ecmin.policy');
-    Route::get('/policy/new', 'PolicyController@newUI')->name('ecmin.policy.new');
-    Route::post('/policy/new', 'PolicyController@new')->name('ecmin.policy.new.do');
-    Route::post('/policy/del', 'PolicyController@delete')->name('ecmin.policy.delete.do');
-    // Statisic income
-    Route::get('/income', 'Statistics@stat')->name('ecmin.income');
-    Route::get('/income/today', 'Statistics@today')->name('ecmin.income.today');
-    Route::get('/income/month', 'Statistics@thisMonth')->name('ecmin.income.thisMonth');
-    Route::post('/income', 'Statistics@statByTime')->name('ecmin.income.byTime');
 });
