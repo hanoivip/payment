b4cdc228ed0c5e9231cd9c9e2f035c372638d528
hanoivip <game.oh.vn@gmail.com>
2021-10-05 11:29:42 +0700

    f

---
+++ lang/en/balance.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Recharge' => 'Old recharge',
+    'TopupPolicy' => 'Sale Off',
+    'Topup' => 'Old topup',
+    'Proceed' => 'Proceed Act',
+    'PaymentChanges' => 'Payment changes',
+    'PaymentRefund' =>'Payment refund',
+    'CreditMethod' => 'Pay by credit',
+];
