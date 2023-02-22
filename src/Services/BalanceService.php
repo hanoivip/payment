255c8a2142a25b81f793f6ee7f42005a76edb1d4
unknown <game.oh.vn@gmail.com>
2023-02-22 13:50:06 +0700

    f

--- src/Services/BalanceService.php
+++ src/Services/BalanceService.php
@@ -8,9 +8,16 @@ use Hanoivip\Payment\Models\Balance;
 use Illuminate\Support\Facades\Log;
 use Hanoivip\Payment\Contracts\IBalance;
 use Exception;
-
+/**
+ * 
+ * @author GameOH
+ * 22/02/2023: Add currency supports
+ * 
+ */
 class BalanceService implements IBalance
 {
+    const MAIN_BALANCE = 0;
+
     /**
      * Truy xuất tất cả các loại tài khoản của ng chơi.
      * 
@@ -24,19 +31,36 @@ class BalanceService implements IBalance
     }
     
     /**
-     * 
+     * Convert amount of money (w/wo currency) into webcoin
+     * - no currency => convert 1-1
+     * - currency => convert into USD * 100
      * @param number $uid
      * @param string $type
      * @param number $value
      * @param string $reason
+     * @param string $currency Source currency
      * @return boolean
      */
-    public function add($uid, $value, $reason, $type = 0)
+    public function add($uid, $value, $reason, $type = 0, $currency = null)
     {
         if ($value <= 0)
         {
             Log::warn("Balance value is zero or negative. skip!");
             return;
+        }
+        if (!empty($currency))
+        {
+            // exchange: $amount => USD * 100
+            $coin = intval(Currency::convert()
+                ->from($currency)
+                ->to('USD')
+                ->amount($value)
+                ->get() * 100);
+            $reason = $reason . ":$currency@$value";
+        }
+        else
+        {
+            $coin = intval($value);
         }
         $balance = Balance::where('user_id', $uid)
                         ->where('balance_type', $type)
@@ -46,22 +70,21 @@ class BalanceService implements IBalance
             $balance = new Balance();
             $balance->user_id = $uid;
             $balance->balance_type = $type;
-            $balance->balance = $value;
+            $balance->balance = $coin;
             $balance->save();
         }
         else
         {
-            $balance->balance += $value;
+            $balance->balance += $coin;
             $balance->save();
         }
-        
+        // save log
         $log = new BalanceMod();
         $log->user_id = $uid;
         $log->balance_type = $type;
-        $log->balance = $value;
+        $log->balance = $coin;
         $log->reason = $reason;
         $log->save();
-        
         return true;
     }
     
@@ -71,14 +94,29 @@ class BalanceService implements IBalance
      * @param string $type Value to substract. Positive value.
      * @param number $value
      * @param string $reason
+     * @param string $currency
      * @return boolean
      */
-    public function remove($uid, $value, $reason, $type = 0)
+    public function remove($uid, $value, $reason, $type = 0, $currency = null)
     {
         if ($value <= 0)
         {
             Log::warn("Balance value is zero or negative. skip!");
             return false;
+        }
+        if (!empty($currency))
+        {
+            // exchange: $amount => USD * 100
+            $coin = intval(Currency::convert()
+                ->from($currency)
+                ->to('USD')
+                ->amount($value)
+                ->get() * 100);
+            $reason = $reason . ":$currency@$value";
+        }
+        else 
+        {
+            $coin = intval($value);
         }
         $balance = Balance::where('user_id', $uid)
             ->where('balance_type', $type)
@@ -88,21 +126,20 @@ class BalanceService implements IBalance
             Log::debug("Balance user {$uid} has not balance type {$type} yet.");
             return false;
         }
-        if ($balance->balance < $value)
+        if ($balance->balance < $coin)
         {
             Log::debug("Balance user {$uid} has not enough balance");
             return false;
         }
-        $balance->balance -= $value;
+        $balance->balance -= $coin;
         $balance->save();
-        
+        // save log
         $log = new BalanceMod();
         $log->user_id = $uid;
         $log->balance_type = $type;
-        $log->balance = -1 * $value;
+        $log->balance = -1 * $coin;
         $log->reason = $reason;
         $log->save();
-        
         return true;
     }
     
@@ -145,50 +182,23 @@ class BalanceService implements IBalance
         return [$objects, ceil($total / 10), $page];
     }
     
-    public function enough($uid, $coin, $type = 0)
+    public function enough($uid, $amount, $type = 0, $currency = null)
     {
         $balance = Balance::where('user_id', $uid)
                         ->where('balance_type', $type)
                         ->first();
+        $coin = $amount;
+        if (!empty($currency))
+        {
+            $coin = intval(Currency::convert()
+                ->from($currency)
+                ->to('USD')
+                ->amount($amount)
+                ->get()) * 100;
+        }
         if (!empty($balance))
             return $balance->balance >= $coin;
         return false;
     }
 
-    public function addCurrency($uid, $amount, $currency, $reason = "", $type = 0)
-    {
-        if (empty($amount) || empty($currency))
-        {
-            throw new Exception("Balance amount and/or currency invalid!");
-        }
-        // exchange: $amount => USD * 100
-        $coin = intval(Currency::convert()
-        ->from($currency)
-        ->to('USD')
-        ->amount($amount)
-        ->get() * 100);
-        $balance = Balance::where('user_id', $uid)
-        ->where('balance_type', $type)
-        ->first();
-        if (empty($balance))
-        {
-            $balance = new Balance();
-            $balance->user_id = $uid;
-            $balance->balance_type = $type;
-            $balance->balance = $coin;
-            $balance->save();
-        }
-        else
-        {
-            $balance->balance += $coin;
-            $balance->save();
-        }
-        $log = new BalanceMod();
-        $log->user_id = $uid;
-        $log->balance_type = $type;
-        $log->balance = $coin;
-        $log->reason = $reason;
-        $log->save();
-        return true;
-    }
 }
