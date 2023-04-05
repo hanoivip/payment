53b0d58c410be0d3453ef8c8b0dd9231545dcbff
unknown <game.oh.vn@gmail.com>
2023-04-05 13:00:07 +0700

    f

--- views/webtopup-result.blade.php
+++ views/webtopup-result.blade.php
@@ -4,13 +4,24 @@
 
 @section('content')
 
-<p>Kết quả thực hiện: {{$data->getDetail()}}</p>
+<p>Payment detail: {{$data->getDetail()}}</p>
 
 @if ($data->isPending())
 <form method="post" action="{{route('webtopup.query')}}">
-{{ csrf_field() }}
-<input type="hidden" id="trans" name="trans" value="{{$trans}}"/>
-	<button type="submit">Cập nhật</button>
+    {{ csrf_field() }}
+    <input type="hidden" id="trans" name="trans" value="{{$trans}}"/>
+    	<button type="submit" class="btn btn-primary">Refresh</button>
 </form>
 @endif
+
+@if ($data->isSuccess())
+<p>Card amount: {{ $data->getAmount() }} {{ $data->getCurrency() }}</p><br/>
+<a href="{{ route('recharge') }}" class="btn btn-primary">Buy game item</a><br/>
+<a href="{{ route('webtopup') }}" class="btn btn-secondary">Pay more</a><br/>
+@endif
+
+@if ($data->isFailure())
+<a href="{{ route('webtopup') }}" class="btn btn-primary"><button>Pay again</button></a>
+@endif
+
 @endsection
