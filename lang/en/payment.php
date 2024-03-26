2a042b7ebf305009dc0433e8734534658209a2d1
unknown <game.oh.vn@gmail.com>
2024-03-26 15:20:57 +0700

    f

--- lang/en/payment.php
+++ lang/en/payment.php
@@ -11,10 +11,14 @@ return [
     ],
     'CreditPaymentMethod' => [
         'guidelines' => 'You can pay with credit in your web account. Credit can be got in many way. For more detail, click link below.',
-        'url' => 'https://bombheroes.us/blog/pay-with-credit-guidelines',
+        'url' => env('APP_URL') . '/blog/pay-with-credit-guidelines',
     ],
     'TsrPaymentMethod' => [
         'guidelines' => 'You can pay with Prepaid Vietnamese Card (game & telco cards). We currently support Zing, Vinaphone & VTC cards. For more detail, click link below',
-        'url' => 'https://bombheroes.us/blog/pay-with-tsr-guidelines',
+        'url' =>  env('APP_URL') . '/blog/pay-with-tsr-guidelines',
     ],
+    'PaytrPaymentMethod' => [
+        'guidelines' => 'You can pay with VISA/Master Card via paytr.com',
+        'url' =>  env('APP_URL') . '/blog/pay-with-paytr-guidelines',
+    ]
 ];
--- lang/pt/payment.php
+++ lang/pt/payment.php
@@ -8,5 +8,17 @@ return [
     ],
     'credit' => [
         'no-point' => 'You have 0 point'
+    ],
+    'CreditPaymentMethod' => [
+        'guidelines' => 'You can pay with credit in your web account. Credit can be got in many way. For more detail, click link below.',
+        'url' => env('APP_URL') . '/blog/pay-with-credit-guidelines',
+    ],
+    'TsrPaymentMethod' => [
+        'guidelines' => 'You can pay with Prepaid Vietnamese Card (game & telco cards). We currently support Zing, Vinaphone & VTC cards. For more detail, click link below',
+        'url' =>  env('APP_URL') . '/blog/pay-with-tsr-guidelines',
+    ],
+    'PaytrPaymentMethod' => [
+        'guidelines' => 'You can pay with VISA/Master Card via paytr.com',
+        'url' =>  env('APP_URL') . '/blog/pay-with-paytr-guidelines',
     ]
 ];
