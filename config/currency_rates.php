f7705b5c721f5cd161c0baedbb62827879196610
unknown <game.oh.vn@gmail.com>
2024-07-26 10:48:22 +0700

    f

--- config/currency_rates.php
+++ config/currency_rates.php
@@ -1,10 +1,21 @@
 <?php
 
 return [
+	// viet dong
     'VND_TO_USD' => 1/23800,
     'USD_TO_VND' => 23800,
     'TL_TO_USD' => 1/29.97,
     'USD_TO_TL' => 29.97,
-    'BLR_TO_USD' => 1/4.91,
+    'BRL_TO_USD' => 1/4.91,
     'USD_TO_BRL' => 4.91,
+	'BRL_TO_TL' => 6.37,
+	'TL_TO_BRL' => 1/6.37,
+	'BRL_TO_VND' => 4988.15,
+	'VND_TO_BRL' => 1/4988.15,
+	// thai baht
+	'USD_TO_THB' => 36.16,
+	'THB_TO_USD' => 1/36.16,
+	// indo rupiah
+	'USD_TO_IDR' => 16295,
+	'IDR_TO_USD' => 1/16295,
 ];
