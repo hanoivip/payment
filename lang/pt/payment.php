13e08c03a376f28e375dfa4292d4f0a055d99fce
unknown <game.oh.vn@gmail.com>
2025-02-27 10:26:07 +0700

    f

--- lang/pt/payment.php
+++ lang/pt/payment.php
@@ -1,24 +1,32 @@
 <?php
 
 return [
-    'methods' => [
-        'next' => 'Next',
-        'title' => 'Please choose payment method',
-        'empty' => 'Still have no payment method. Plz try later.'
-    ],
-    'credit' => [
-        'no-point' => 'You have 0 point'
-    ],
-    'CreditPaymentMethod' => [
-        'guidelines' => 'You can pay with credit in your web account. Credit can be got in many way. For more detail, click link below.',
-        'url' => env('APP_URL') . '/blog/pay-with-credit-guidelines',
-    ],
-    'TsrPaymentMethod' => [
-        'guidelines' => 'You can pay with Prepaid Vietnamese Card (game & telco cards). We currently support Zing, Vinaphone & VTC cards. For more detail, click link below',
-        'url' =>  env('APP_URL') . '/blog/pay-with-tsr-guidelines',
-    ],
-    'PaytrPaymentMethod' => [
-        'guidelines' => 'You can pay with VISA/Master Card via paytr.com',
-        'url' =>  env('APP_URL') . '/blog/pay-with-paytr-guidelines',
-    ]
+	'methods' => [
+		'next' => 'Próximo',
+		'title' => 'Por favor, escolha o método de pagamento',
+		'empty' => 'Ainda não tenho um método de pagamento. Tente mais tarde.'
+		],
+	'credit' => [
+		'no-point' => 'Você tem 0 pontos',
+		],
+	'CreditPaymentMethod' => [
+		'guidelines' => 'Você pode pagar com crédito em sua conta da web. O crédito pode ser obtido de várias maneiras. Para mais detalhes, clique no link abaixo.',
+		'url' => env('APP_URL') . '/blog/pay-with-credit-guidelines',
+		],
+	'TsrPaymentMethod' => [
+		'guidelines' => 'Você pode pagar com cartão vietnamita pré-pago (cartões de jogo e telecomunicações). Atualmente, oferecemos suporte aos cartões Zing, Vinaphone e VTC. Para mais detalhes, clique no link abaixo',
+		'url' => env('APP_URL') . '/blog/pay-with-tsr-guidelines',
+		],
+	'PaytrPaymentMethod' => [
+		'guidelines' => 'Você pode pagar com VISA/Master Card via paytr.com',
+		'url' => env('APP_URL') . '/blog/pay-with-paytr-guidelines',
+		],
+	'PaypalPaymentMethod' => [
+		'guidelines' => 'Você pode pagar com sua conta bancária',
+		'url' => env('APP_URL') . '/blog/pay-with-paypal-guidelines',
+		],
+	'MercadoPaymentMethod' => [
+		'guidelines' => 'Você pode pagar via Mercado Pago',
+		'url' => env('APP_URL') . '/blog/pay-with-mercado-guidelines',
+		]
 ];
